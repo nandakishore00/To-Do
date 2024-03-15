81be5e41fced14deed790f02b097e513d1f44241
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2024-03-14 22:32:28 -0400

    create to-do list file

---
+++ src/to-do list app.js
@@ -0,0 +1,5 @@
+const toDoArray=[]
+const createToDo=(Title, Description, DueDate,Priority,Checklist)=>{
+toDoArray.push(Title, Description, DueDate,Priority,Checklist);
+return Title, Description, DueDate,Priority,Checklist;
+}
