eb03da1016ed7c619b63c8a2f8aed8f75c2455c6
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2024-04-06 01:42:30 -0400

    allow user to add taskitem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import createToDo from './to-do list'
+const forms = document.querySelector("form");
+const newTaskButton = document.querySelector(".newTaskButton");
+const newTaskInput = document.getElementById('newTaskInput');
+const taskList = document.querySelector(".taskList");
+function handleEnter() {
+    const taskTitle = newTaskInput.value;
+    if (taskTitle !== '') {
+      const newTaskItem = document.createElement('li');
+      newTaskItem.textContent = taskTitle;
+      taskList.appendChild(newTaskItem);
+      newTaskInput.value = '';
+      forms.classList.add('hidden');
+    }
 
-const toDo=createToDo("Project","To-do list","14/04/2024","Medium","delete task","create task","customize task")
+  }
+
+newTaskButton.addEventListener('click', () => {
+  forms.classList.toggle('hidden');
+});
+
+newTaskInput.addEventListener('keydown', (e) => {
+    e.preventDefault()
+    if (e.key==='Enter'){handleEnter();}
+  
+});
--- src/project.js
+++ src/project.js
@@ -1,1 +1,2 @@
+function project(){
 export default project
