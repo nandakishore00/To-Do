2a0fb473f8eae6b0e8f6bca158a8dbf4e6c079cb
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2024-04-06 16:34:16 -0400

    add edit and delete icons to list of to-do's

--- src/project.js
+++ src/project.js
@@ -4,27 +4,34 @@ const newTaskButton = document.querySelector(".newTaskButton");
 const newTaskInput = document.getElementById('newTaskInput');
 const taskList = document.querySelector(".taskList");
 function handleEnter() {
-    
     const taskTitle = newTaskInput.value;
-    if (taskTitle !== '') {
+    if (taskTitle !== ''){
+      const content=document.createElement('span')
       const newTaskItem = document.createElement('li');
-      newTaskItem.textContent = taskTitle;
+      const delIcon=document.createElement('button');
+      const editIcon=document.createElement('button');
+      const itemIcons=document.createElement('div');
+      content.classList.add('content')
+      delIcon.classList.add('fas','fa-trash');
+      editIcon.classList.add('far','fa-edit');
+      newTaskItem.classList.add('newTaskItem')
+      itemIcons.classList.add('itemIcons')
+      content.textContent = taskTitle;
+      itemIcons.appendChild(editIcon);
+      itemIcons.appendChild(delIcon);
+      newTaskItem.appendChild(content);
+      newTaskItem.appendChild(itemIcons);
       taskList.appendChild(newTaskItem);
       newTaskInput.value = '';
       forms.classList.add('hidden');
     }
-
   }
-
 newTaskButton.addEventListener('click', () => {
   forms.classList.toggle('hidden');
 });
-
-newTaskInput.addEventListener('keydown', (e) => {
-    
+newTaskInput.addEventListener('keydown', (e) => {  
     if (e.key==='Enter'){
         e.preventDefault()
         handleEnter();}
-  
 });}
 export {project}
